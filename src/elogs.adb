76b15501ee99b21496f0a9de444fcb96840b1e8f
Kevin Chadwick <kc-github@chadwicks.me.uk>
2023-12-18 15:49:02 +0000

    Add license text

--- src/elogs.adb
+++ src/elogs.adb
@@ -183,3 +183,20 @@ is
    end Latest_Message;
 
 end Elogs;
+
+ISC License (Simplified BSD)
+
+Copyright (c) 2023, Kevin Chadwick
+Copyright (c) 2023, Elansys Limited
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
